05ca4f9bf7353c2f6e12080fa7e0e3a9bd6816fb
charun620 <51948655+charun620@users.noreply.github.com>
2019-06-25 16:09:42 +0700

    Add files via upload

---
+++ 111.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+	cout << "Studen Code : 62-060216-2217-8 \n";
+	cout << "Studen Name : Charun Promsopha\n";
+	cout << "Divition    : Infomation Technology\n";
+	cout << "Year        : 1\n";
+	cout << "Room        : RC\n";
+	cout << "Sex         : Male\n";
+	cout << "Age         : 18\n";
+
+}
---
+++ L1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	cout << "\"Infomation Technology\'C\'\" \n" << endl;
+    cout << setw(65) <<"I want to be Programmer" << endl;
+	cout << setw(175) << "Hello\n";
+	return 0;
+
+}
